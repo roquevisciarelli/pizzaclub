ae62d8903384a5c8cb1846f8120c84d68c8dbb31
roquevisciarelli <rvisciarelli2@gmail.com>
2026-06-25 00:15:33 -0300

    admin pestania error

--- PizzaClub/frontend/admin.js
+++ PizzaClub/frontend/admin.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_BASE_URL = 'https://pizzaclub-rjeq.onrender.com';
+    const API_BASE_URL = 'https://pizzaclub-rjeq.onrender.com/api/auth/login';
 
     // Elementos del DOM
     const loginContainer = document.getElementById('login-container');
